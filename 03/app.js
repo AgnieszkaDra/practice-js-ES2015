eed8ab697a7b3cfa3859b722b61444758ff5e8ef
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-12-01 02:27:19 +0100

    end task 03

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,42 @@
 import {files} from './data.js';
 
+function convertToBytes(length, unit){
+    const data = {
+        'B' : 1,
+        'KB' : 1024,
+        'MB' : 1024 * 1024,
+        'BG' : 1024 * 1024
+    }
+
+    if(typeof data[unit] === undefined){
+        console.error('Nie mam takiej jednostki')
+    }
+    
+    return data[unit] * length
+
+   
+
+
+    if(unit === "B"){
+        console.log(length, '??')
+        return length
+    } else if (unit === "KB"){
+        return length * 1024
+    } else if (unit === "MB"){
+        return length * 1024 *1024
+    } else if(unit === "GB"){
+        return length * 1024 * 1024
+    }
+}
+
+let size = 0;
+// files.forEach(f => {
+//     // size += convertToBytes(f.size.length, f.size.unit)
+//     const { size: {length, unit}} = f
+//     size += convertToBytes(length, unit)
+// })
+files.forEach(({ size: {length, unit = "B"}})=> {
+   size += convertToBytes(length, unit)
+})
+
+console.log(size)
